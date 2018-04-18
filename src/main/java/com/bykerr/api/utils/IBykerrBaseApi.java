c2ba620c7e13fd66eba88fa66408c30e38603666
Manojkumar Mathiarasan <manojkumarmathiarasan@gmail.com>
2018-04-18 17:16:35 +0530

    initial commit

--- src/main/java/com/bykerr/api/utils/IBykerrBaseApi.java
+++
@@ -1,8 +0,0 @@
-package com.bykerr.api.utils;
-
-/**
- * Created by mmathiarasan on 16-04-2018.
- */
-
-public interface IBykerrBaseApi {
-}
--- src/main/java/com/bykerr/models/IBykerrReceiverModel.java
+++
@@ -1,8 +0,0 @@
-package com.bykerr.models;
-
-/**
- * Created by mmathiarasan on 17-04-2018.
- */
-
-public class IBykerrReceiverModel {
-}
