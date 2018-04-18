03da6012de5e8d1cbe810a67eefe721865357fac
Manojkumar Mathiarasan <manojkumarmathiarasan@gmail.com>
2018-04-18 17:39:36 +0530

    fixes

--- mce/src/main/java/com/keystrokes/makescodeeasy/core/MCEActivity.java
+++ mce/src/main/java/com/keystrokes/makescodeeasy/core/MCEActivity.java
@@ -1,6 +1,7 @@
 package com.keystrokes.makescodeeasy.core;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.view.Menu;
